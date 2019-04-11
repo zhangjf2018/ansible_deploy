6b35c28d0f36df79d683bfe5e29db2963b9720a3
zjf <414512194@qq.com>
2019-04-11 09:25:44 +0800

    k8s

--- shell/k8s_status.sh
+++ shell/k8s_status.sh
@@ -2,3 +2,11 @@
 kubectl get cs
 kubectl get node
 
+echo ""
+echo "##########获取controller manager leader节点##############"
+kubectl get endpoints kube-controller-manager --namespace=kube-system  -o yaml
+
+echo ""
+echo "##########获取scheduler leader节点###############"
+kubectl get endpoints kube-scheduler --namespace=kube-system  -o yaml
+
